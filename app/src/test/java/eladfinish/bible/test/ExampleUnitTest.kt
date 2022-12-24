b5a88638edb6a95e7d1deb0ec6271ba26cea574b
Elad Finish <elfifo4@gmail.com>
2022-12-24 21:15:08 +0200

    clean unneeded files

--- app/src/test/java/eladfinish/bible/test/ExampleUnitTest.kt
+++ app/src/test/java/eladfinish/bible/test/ExampleUnitTest.kt
@@ -1,24 +1,8 @@
 package eladfinish.bible.test
 
-import org.junit.Test
-
-import org.junit.Assert.*
 import java.io.File
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-//class ExampleUnitTest {
-//    @Test
-//    fun addition_isCorrect() {
-//        assertEquals(4, 2 + 2)
-//    }
-//}
-
 fun main(){
-    println("elad_finish")
     val directory = "formatted"
     val number = "151450"
     val type = ".json"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,28 +7,11 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-/*
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
- */
 
-//rootProject.name = "BibleData"
 include (":app")
