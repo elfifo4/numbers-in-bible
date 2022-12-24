b37c97e210df5591f648a3adb04ec1fc60325110
Elad Finish <elfifo4@gmail.com>
2022-12-24 21:43:38 +0200

    add 186400.json

--- app/src/test/java/eladfinish/bible/data/ExampleUnitTest.kt
+++ app/src/test/java/eladfinish/bible/data/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package eladfinish.bible.test
+package eladfinish.bible.data
 
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
@@ -6,7 +6,7 @@ import java.io.File
 
 fun main() {
     val directory = "formatted"
-    val number = "151450"
+    val number = "186400"
     val type = ".json"
     val bufferedReader = File("$directory/$number$type").bufferedReader(charset = Charsets.UTF_16)
     val inputString = bufferedReader.use { it.readText() }
