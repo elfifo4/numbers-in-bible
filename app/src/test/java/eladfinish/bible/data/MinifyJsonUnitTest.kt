c16d23b444051c43ab56236f136bb143a77ffc33
Elad Finish <elfifo4@gmail.com>
2023-01-10 09:06:05 +0200

    fix for 31

--- app/src/test/java/eladfinish/bible/data/MinifyJsonUnitTest.kt
+++ app/src/test/java/eladfinish/bible/data/MinifyJsonUnitTest.kt
@@ -4,9 +4,12 @@ import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
 import java.io.File
 
+//<b>number</b>
+//<b>וְאֶחָֽד</b>
+
 fun main() {
     val directory = "formatted"
-    val number = "157600"
+    val number = "31"
     val type = ".json"
     val bufferedReader = File("$directory/$number$type").bufferedReader(charset = Charsets.UTF_16)
     val inputString = bufferedReader.use { it.readText() }
